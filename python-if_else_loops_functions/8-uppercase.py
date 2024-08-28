e4b3f16c2838e9aa3516c55319238aff7627c507
Rain Nyte <rain_nyte@proton.me>
2024-08-28 16:02:11 +0000

    function to convert all characters in a string to uppercase and print the result

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for c in str:
+        if ord(c) in range(97,123):
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print("{}".format(result))
