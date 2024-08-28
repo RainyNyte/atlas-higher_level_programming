7f1a56da175a27b3b89451b81faca79055cfa8e7
Rain Nyte <rain_nyte@proton.me>
2024-08-28 18:50:52 +0000

    python script to print the alphabet in reverse alternating in case starting with lowercase

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(26):
-    print("{}".format(chr(ord('z') - i) if i % 2 == 0 \
+    print("{}".format(chr(ord('z') - i) if i % 2 == 0
                       else chr(ord('Z') - i)), end="")
