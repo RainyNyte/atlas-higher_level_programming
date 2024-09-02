c0de0068ac256a06ccfe19ac7f58c9f5791304b5
Rain Nyte <rain_nyte@proton.me>
2024-09-02 20:21:52 +0000

    import all functions in calculator_1.py and run each of the functions in it

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1
+    a = 10
+    b = 5
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
+    result = sub(a, b)
+    print(f"{a} - {b} = {result}")
+    result = mul(a, b)
+    print(f"{a} * {b} = {result}")
+    result = div(a, b)
+    print(f"{a} / {b} = {result}")
