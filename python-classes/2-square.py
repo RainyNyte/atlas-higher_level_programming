9793e952d752de5aaaf0aa0d1f63b50a98378798
Rain Nyte <rain_nyte@proton.me>
2024-09-11 14:51:56 +0000

    class to define a square with attribute size, raises typeerror is size is not an int and value error if size is less than 0

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that defines a class that represents a square."""
+
+
+class Square:
+    """Class that represents a square.
+    Attributes:
+        __size (int): the size of one side of the square.
+    """
+    def __init__(self, size=0):
+        """Initializes the new Square instance with the given size.
+        Args:
+            size (int): The size of one side of the square.
+        Raises:
+            TypeError: If size not an integer.
+            ValueError: If size less than 0.
+        """
+        if not isInstance(size, int):
+            raise TypeError("size must be integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
