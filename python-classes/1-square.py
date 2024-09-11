941698d8f2a5f83ee8073ad16f59b2bf26ec0a78
Rain Nyte <rain_nyte@proton.me>
2024-09-11 14:45:50 +0000

    class to define a square with attribute size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,14 +3,12 @@
 
 
 class Square:
-    """Class that represents a square
-    
+    """Class that represents a square.
     Attributes:
         __size (int): the size of one side of the square
     """
     def __init__(self, size):
-        """Initializes the new Square instance with the given size
-
+        """Initializes the new Square instance with the given size.
         Args:
             size (int): The size of one side of the square
         """
