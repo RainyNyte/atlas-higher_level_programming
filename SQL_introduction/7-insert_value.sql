3ddfaf623c263a9885a409795ac69abc012734a0
Rain Nyte <rain_nyte@proton.me>
2024-10-11 00:22:04 -0500

    fixed syntax of script for inserting a new row into table first_table

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,6 +1,3 @@
 -- insert a new row into table 'first_table'
 
-INSERT INTO TABLE first_table VALUES (
-	id = 89
-	name = 'Best School'
-);
+INSERT INTO TABLE first_table (id, name) VALUES (89, 'Best School');
