3e70264f61c77f99366ae72f000afde964b874d5
Rain Nyte <rain_nyte@proton.me>
2024-10-15 22:54:15 -0500

    creates user_0d_1 and grants them all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 --creates a user user_0d_1 and grants them all permisions
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
