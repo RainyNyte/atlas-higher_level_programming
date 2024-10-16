46654ccb6b5b622e6fc68e5db5d84d5fa3da13dc
Rain Nyte <rain_nyte@proton.me>
2024-10-15 23:59:40 -0500

    create database hbtn_0d_2 and user user_0d_2 with SELECT privilege in hbtn_0d_02

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create database hbtn_0d_2 and user user_0d_2 with SELECT privilege in hbtn_0d_02
+
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
