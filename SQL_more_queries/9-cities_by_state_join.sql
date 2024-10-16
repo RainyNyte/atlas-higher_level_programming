c9a487616316b51f08ec502454887720927b503b
Rain Nyte <rain_nyte@proton.me>
2024-10-16 01:14:17 -0500

    lists all cities contained in the database hbtn_0d_usa

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT id, name, (SELECT name FROM states WHERE id = cities.state_id) AS name FROM cities ORDER BY id ASC;
+SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
